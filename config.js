862343516b43b6224de1375e2201a0fc656b595e
Marie Clemessy <marie.clemessy@gmail.com>
2015-01-22 12:03:05 -0500

    Fixed decoding of IP addresses in case of a binary model answer from the tracker

--- config.js
+++ config.js
@@ -4,7 +4,10 @@ var path = require('path'),
 config = {
 	link: "https://archive.org/download/tristanandisolda16250gut/tristanandisolda16250gut_archive.torrent",
 	download_path: '/Users/marieclemessy/Development/bitMarie/Downloads',
-	port: 6881
+	port: 6881,
+	// info_hash: '%2B%15%CA%2B%FDH%CD%D7m9%ECU%A3%AB%1B%8AW%18%0A%09',
+	info_hash: 'S%aa%90%5eK%f7%89d%22%01%0a%c2i%d7Yoz%24%0cg',
+	peer_id: '-AM1989-A%8e%3a%85%887%7f%afs%f8%60%da'
 }
 
 module.exports = config;

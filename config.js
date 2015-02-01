07678e44e588c8baa53cecfea4ac9603320ea0cf
Marie Clemessy <marie.clemessy@gmail.com>
2015-02-01 17:40:21 -0500

    Removed un-necessary variables

--- config.js
+++ config.js
@@ -2,11 +2,8 @@ var path = require('path'),
 	config;
 
 config = {
-	link: "https://archive.org/download/tristanandisolda16250gut/tristanandisolda16250gut_archive.torrent",
 	download_path: '/Users/marieclemessy/Development/bitMarie/Downloads',
 	port: 6881,
-	info_hash: 'S%aa%90%5eK%f7%89d%22%01%0a%c2i%d7Yoz%24%0cg',
-	peer_id: '-AM1989-A%8e%3a%85%887%7f%afs%f8%60%da'
 }
 
 module.exports = config;
